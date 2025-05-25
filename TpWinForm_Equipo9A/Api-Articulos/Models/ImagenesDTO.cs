9fa73d2c1345dc1c989a0f1626597a10e5954908
MRS-HUB <marcoscolombres203@gmail.com>
2025-05-24 22:15:32 -0300

    Agregue funcionalidad para agregar imagenes

---
+++ TpWinForm_Equipo9A/Api-Articulos/Models/ImagenesDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dominio;
+
+
+namespace Api_Articulos.Models
+{
+    public class ImagenesDTO
+    {
+        public int Id { get; set; }
+        public List<imagenDTO> imagenDTOs { get; set; }
+}
+}
